e605c9afcd1d5f19ea4c2fc565a2025633758ecc
Adem Koca <120526227+admkcaa@users.noreply.github.com>
2023-10-05 18:31:26 +0300

    Add files via upload

---
+++ 1-Expanding Cards/app.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+
+        removeActiveClassess()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClassess(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
